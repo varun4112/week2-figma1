ac4067afba5b2040e1a8acc1fc376b8e9b63d871
varun <varunnidumm@gmail.com>
2024-08-17 11:11:17 +0530

    ui

--- resources/script.js
+++ resources/script.js
@@ -28,6 +28,8 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
+// SIDEBAR OPEN AND CLOSE
+
 document.addEventListener("DOMContentLoaded", function () {
   const burger = document.querySelector(".burger");
   const sidebar = document.querySelector(".side-bar");
@@ -35,6 +37,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const BookACar = document.getElementById("BookACar");
   const bookTabs = document.getElementById("bookTabs");
 
+  // SAME AND DIFFERENT AS PICKUP
   const sameAsPickUp = document.getElementById("sameAsPickUp");
   const differentPickup = document.getElementById("differentPickup");
   const dropOffLocation = document.getElementById("dropOffLocation");
@@ -113,3 +116,26 @@ document
       // Handle form submission here
     }
   });
+
+// Slide show
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
